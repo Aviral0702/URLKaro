7d8104ecd4f9a475fcf0616e4086f5a7d555fc17
root <root@Aviral>
2024-10-15 00:32:06 +0530

    added backend functionality for shortening url

---
+++ src/pages/api/create.js
@@ -0,0 +1,28 @@
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import { nanoid } from "nanoid";
+
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_API_KEY
+)
+
+export default async function handler(req,res) {
+    if(req.method=='POST'){
+        const {originalUrl} = req.body;
+        const id = nanoid(8);
+        
+        const {data,error} = await supabase
+        .from('urls')
+        .insert([{id,original_url:originalUrl}])
+        .single();
+
+        if(error) {
+            return res.status(500).json({error: 'Failed to create short URL'});
+        } else {
+            const shortUrl = `${req.headers.host}/${data.id}`;
+            res.status(201).json({shortUrl})
+        }
+    } else {
+        res.status(500).json({error: 'Method not allowed'});
+    }
+}
