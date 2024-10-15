6da4e9cdd0413a54ed6bb0216cfbeded914bd96c
Aviral Asthana <aviral.asthana0704@gmail.com>
2024-10-16 02:06:52 +0530

    Refactor next.config.mjs and package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/URLKaro/' : '',
+  basePath: isProd ? '/URLKaro' : '',
+  output: 'export'
 };
 
 export default nextConfig;
